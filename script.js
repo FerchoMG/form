bb2d669ade8f62a7bdf798efae49b8976c3523cf
Mer <paolamerly1@gmail.com>
2022-11-23 01:08:31 -0500

    complete

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const d = document,
-  $modal = d.querySelector(".modal");
+  $modal = d.querySelector(".modal"), $reset = d.querySelector('#reset'),
+   $form = d.querySelector('.form'),
+  $checkbox = d.querySelectorAll('.checkbox_m');
+
+d.addEventListener('DOMContentLoaded', e=>{
+  $checkbox.forEach(el=>{el.checked = false})
+
+})
 
 d.addEventListener("click", (e) => {
+  if(e.target.matches('#submit')){
+    e.preventDefault()
+  }
+  if(e.target.matches('#reset')){
+    location.reload()
+  }
   if (e.target.matches(".burger")) {
     if (!$modal.classList.contains("modal-on")) {
       $modal.classList.add("modal-on");
@@ -10,3 +23,20 @@ d.addEventListener("click", (e) => {
     }
   }
 });
+
+function validarContraseña() {
+  var pass = __('pass').value,
+      pass2 = __('pass2').value;
+  if(pass != '' && pass2 != ''){
+    if(pass != pass2){
+      //si las contraseñas no coinciden
+      __('resultado').innerHTML = '<p class="error"><strong>Error: </strong>¡Las contraseñas no coinciden!</p>';
+    } else {
+      //Si todo esta correcto 
+      __('form').innerHTML = '<p class="correcto"><strong>✓ Correcto: </strong>Los datos son correctos :)</p>';
+    }
+  } else {
+    //si los campos o uno, este vacio
+    __('resultado').innerHTML = '<p class="error"><strong>Error: </strong>¡Los campos no deben estar vacios!</p>';
+  } 
+}
